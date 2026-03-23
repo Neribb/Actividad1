f7f79b673355f7dc1d3121f3f22341f06b22b08a
Neribb <neri.unam16@gmail.com>
2026-03-22 19:33:59 -0600

    Delete Calcualdora directory

--- Calcualdora/app/src/main/java/com/example/calculadora/ICalculadora.java
+++
@@ -1,6 +0,0 @@
-package com.example.calculadora;
-
-public interface ICalculadora {
-    double realizarOperacion(double a, double b, Operador op);
-    double resolverExpresion(String expresion);
-}
